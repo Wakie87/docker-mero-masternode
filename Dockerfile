d0bc58bd1181a3a820ba33a5ad23ddbc9f9eed5a
Scott Wakefield <scott@npclinics.com.au>
2018-07-13 10:19:14 +0800

    updates

---
+++ Dockerfile
@@ -0,0 +1,46 @@
+## Base Image
+FROM ubuntu:16.04
+
+## Environment Variables
+# ENV TMP_FOLDER=$(mktemp -d)
+ENV CONFIG_FILE='mero.conf'
+ENV CONFIGFOLDER='/root/.mero'
+ENV COIN_DAEMON='/usr/local/bin/merod'
+ENV COIN_CLI='/usr/local/bin/mero-cli'
+ENV COIN_REPO='https://github.com/merocoin/mero/releases/download/v1.0.0/linux_x64.tar.gz'
+ENV COIN_NAME='Mero'
+ENV COIN_PORT=14550
+ENV CONFIG_FILE='mero.conf'
+ENV CONFIGFOLDER='/root/.mero'
+ENV COINKEY=""
+
+# ENV NODEIP=$(curl -s4 icanhazip.com)
+ENV RED='\033[0;31m'
+ENV GREEN='\033[0;32m'
+ENV NC='\033[0m'
+
+## Install Dependencies
+RUN apt-get upgrade && apt-get update && apt-get install -y \
+    software-properties-common
+RUN apt-add-repository -y ppa:bitcoin/bitcoin
+RUN apt-get update && apt-get install -y \
+    build-essential libtool autoconf libssl-dev libboost-dev sudo automake \
+    make wget curl unzip ufw git pkg-config net-tools \ 
+    libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev \
+    libboost-system-dev libboost-test-dev libboost-thread-dev \
+    libdb4.8-dev bsdmainutils libdb4.8++-dev libminiupnpc-dev \
+    libgmp3-dev libzmq3-dev libevent-dev libdb5.3++
+
+## Download Binaries and copy to system folder
+WORKDIR /root
+RUN wget -nv -O - $COIN_REPO | tar xvz -C /root
+RUN cp /root/mero* /usr/local/bin \
+    && strip /usr/local/bin/merod /usr/local/bin/mero-cli \
+    && chmod +x /usr/local/bin/merod && chmod +x /usr/local/bin/mero-cli \
+    && rm -rf /root/mero*
+
+WORKDIR /root
+COPY ./mero_install.sh ./
+RUN chmod +x /root/mero_install.sh
+CMD [ "mero_install.sh" ]
+ENTRYPOINT [ "bash" ]
---
+++ mero_install.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+TMP_FOLDER=$(mktemp -d)
+NODEIP=$(curl -s4 icanhazip.com)
+
+function create_config() {
+  echo -e "Creating Config..."
+  mkdir $CONFIGFOLDER
+  RPCUSER=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w10 | head -n1)
+  RPCPASSWORD=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w22 | head -n1)
+  cat << EOF > $CONFIGFOLDER/$CONFIG_FILE
+rpcuser=$RPCUSER
+rpcpassword=$RPCPASSWORD
+rpcallowip=127.0.0.1
+listen=1
+server=1
+daemon=1
+port=$COIN_PORT
+EOF
+}
+
+function update_config() {
+  echo -e "Updating Config..."
+  sed -i 's/daemon=1/daemon=0/' $CONFIGFOLDER/$CONFIG_FILE
+  cat << EOF >> $CONFIGFOLDER/$CONFIG_FILE
+logintimestamps=1
+maxconnections=256
+#bind=$NODEIP
+masternode=1
+externalip=$NODEIP:$COIN_PORT
+masternodeprivkey=$COINKEY
+EOF
+}
+
+function get_ip() {
+  declare -a NODE_IPS
+  for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
+  do
+    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
+  done
+
+  if [ ${#NODE_IPS[@]} -gt 1 ]
+    then
+      echo -e "${GREEN}More than one IP. Please type 0 to use the first IP, 1 for the second and so on...${NC}"
+      INDEX=0
+      for ip in "${NODE_IPS[@]}"
+      do
+        echo ${INDEX} $ip
+        let INDEX=${INDEX}+1
+      done
+      read -e choose_ip
+      NODEIP=${NODE_IPS[$choose_ip]}
+  else
+    NODEIP=${NODE_IPS[0]}
+    echo "IP Address.. $NODEIP"
+  fi
+}
+
+function compile_error() {
+if [ "$?" -gt "0" ];
+ then
+  echo -e "${RED}Failed to compile $COIN_NAME. Please investigate.${NC}"
+  exit 1
+fi
+}
+
+function important_information() {
+ echo
+ echo -e "================================================================================================================================"
+ echo -e "$COIN_NAME Masternode is up and running listening on port ${RED}$COIN_PORT${NC}."
+ echo -e "Configuration file is: ${RED}$CONFIGFOLDER/$CONFIG_FILE${NC}"
+ echo -e "Start: ${RED}systemctl start $COIN_NAME.service${NC}"
+ echo -e "Stop: ${RED}systemctl stop $COIN_NAME.service${NC}"
+ echo -e "VPS_IP:PORT ${RED}$NODEIP:$COIN_PORT${NC}"
+ echo -e "MASTERNODE PRIVATEKEY is: ${RED}$COINKEY${NC}"
+ if [[ -n $SENTINEL_REPO  ]]; then
+  echo -e "${RED}Sentinel${NC} is installed in ${RED}/sentinel${NC}"
+  echo -e "Sentinel logs is: ${RED}$CONFIGFOLDER/sentinel.log${NC}"
+ fi
+ echo -e "Please check ${RED}$COIN_NAME${NC} is running with the following command: ${RED}systemctl status $COIN_NAME.service${NC}"
+ echo -e "================================================================================================================================"
+}
+
+function setup_node() {
+  echo -e "Setting up..."
+  get_ip
+  create_config
+  update_config
+}
+
+function run_node() {
+  echo -e "Running node..."
+  merod -daemon -conf=$CONFIGFOLDER/$CONFIG_FILE -datadir=$CONFIGFOLDER
+  tail -f /root/.mero/debug.log
+}
+
+##### Main #####
+setup_node
+run_node
